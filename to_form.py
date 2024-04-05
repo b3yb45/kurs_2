490ad3ab492b5a9c5f46a281d49abe20e0304d86
b3yb45 <beybaset@gmail.com>
2024-04-05 10:31:10 +0700

    update to form

--- to_form.py
+++ to_form.py
@@ -2,8 +2,8 @@ import requests
 from json_excel_converter import Converter 
 from json_excel_converter.xlsx import Writer
 
-endpoint = 'https://budget.gov.ru/epbs/registry/7710568760-EXECUTBUDGET/data'
-query_params = {"filterperiod": "01012019", "filterperiodicity": "Y", "pageSize": "100", "filteroktmo.oktmoname": "Новосибирская область", "blocks": "PERKB", "FORMCODEOKUD": "0503317"}
+endpoint = 'http://budget.gov.ru/epbs/registry/7710568760-repexecutincome/data'
+query_params = {"filterperiod": "01012019", "filterperiodicity": "Y", "pageSize": "10000", "filteroktmo.oktmoname": "Новосибирская область", "blocks": "PERKB", "FORMCODEOKUD": "0503317"}
 resp = requests.get(endpoint, params=query_params)
 
 conv = Converter()
